39fe550e78a870402c27ec8a4e5a14d956b3599b
Ash - SIlverless <ash@silverless.co.uk>
2019-06-06 16:37:53 +0100

    DEV - fixed sticky

--- inc/js/core.js
+++ inc/js/core.js
@@ -35,7 +35,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top -150 }, 1000);
+          scrollTop: target.offset().top -250 }, 1000);
         return false;
       }
     }
