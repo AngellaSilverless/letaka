1f13ac2955f5d84b8647fe4bef216de7b0051bc3
Angella <>
2019-06-06 10:26:01 +0100

    DEV - News page

--- page-templates/home.php
+++ page-templates/home.php
@@ -117,31 +117,15 @@ get_header();?>
 					)
 				);
 				
-				if($news): foreach($news as $post): setup_postdata($post); ?>
+				if($news): foreach($news as $post): setup_postdata($post);
 				
-				<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id(), "large"); ?>
-				
-				<div class="img" style="background: url(<?php echo $image[0]; ?>)"></div>
-				
-				<div class="content">
-				
-					<span class="font700 heading__secondary-color"><?php the_date("d.m.Y"); ?></span>
+					set_query_var('newsletter', true);
 					
-					<h3 class="heading heading__primary-color size2 brand-line"><?php the_title(); ?></h3>
-					
-					<div class="description mt1 mb1"><?php echo substr(wp_strip_all_tags(get_the_content()), 0, 300) . "..."; ?></div>
-					
-					<div class="action-buttons">
-						
-						<div><a class="button button__transparent" href="<?php the_permalink(); ?>">Read More</a></div>
-						
-						<div><a class="button button__secondary-color" href="<?php the_permalink(); ?>">Newsletter Sign Up</a></div>
-						
-					</div>
-					
-					<?php endforeach; wp_reset_postdata(); endif; ?>
+					get_template_part('template-parts/news');
 				
-				</div>
+				endforeach; wp_reset_postdata(); endif;
+				
+				?>
 				
 			</div>
 			
