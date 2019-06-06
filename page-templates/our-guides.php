e645f0acb1aef95188efa767dc1b6c1f74998b3a
Angella <>
2019-06-06 16:01:53 +0100

    DEV - Our Guides page

--- page-templates/our-guides.php
+++ page-templates/our-guides.php
@@ -34,11 +34,66 @@ get_header();?>
 		    
 		    <div class="col-9">
 	        
-		        <div class="main justify">
-			        
-			        <?php the_field("content"); ?>
-			        
-		        </div>
+			    <?php
+				
+				$guides = get_terms("guide");
+				
+				foreach($guides as $guide): ?>
+				
+				<div class="wrapper-guides">
+							
+					<div class="guide heading heading__sm">
+						
+						<?php 
+							
+						$image = get_field('image', $guide->taxonomy . '_' . $guide->term_id);
+						
+						?>
+						
+						<div class="img mr1" style="background:url(<?php echo $image["sizes"]["thumbnail"]; ?>);"></div><?php
+							
+						echo $guide->name;
+						
+						?><i class="fas fa-chevron-right"></i>
+						
+					</div>
+					
+					<div class="info" style="display:none;">
+						
+						<div class="row">
+							
+							
+						<div><div class="col-4 img" style="background:url(<?php echo $image["url"]; ?>);"></div><?php
+							
+							the_field('description', $guide->taxonomy . '_' . $guide->term_id);
+						
+						?></div>
+							
+						<?php 
+							
+						$images = get_field('gallery', $guide->taxonomy . '_' . $guide->term_id);
+						
+						if( $images ): ?>
+		
+						<div class="gallery">
+						
+							<?php foreach( $images as $image ): ?>
+							
+							<a href="<?php echo $image['url']; ?>" class="lightbox-gallery"  alt="<?php echo $image['alt']; ?>" style="background-image: url(<?php echo $image['url']; ?>);"></a>
+							
+							<?php endforeach; ?>
+						
+						</div>
+						
+						<?php endif; ?>
+						
+					</div>
+					
+				</div>
+				
+				</div>
+				
+				<?php endforeach; ?>
 		    
 		    </div>
 	    
