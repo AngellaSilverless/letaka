4a064b9724840f848fb64aa78a6a7518cf275eec
Angella <>
2019-08-14 13:42:45 +0100

    FIX - Posts heading align

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -16,55 +16,69 @@
         <?php if ( is_front_page() ): ?>
         
 			<div class="col-12 col-sm-8 offset-0 offset-sm-2">
+				
                 <div class="hero__content">
 			
-			<div>
-			
-				<img class="slide-down img-logo" src="<?php echo get_template_directory_uri() . "/inc/img/letaka-logo.svg"; ?>">
+					<div>
 				
-				<h1 class="heading heading__xl heading__light"><?php the_field( 'hero_heading' );?></h1>
-			
-			</div>
-			
-			<div>
-			
-				<div class="video-icon mt1 mb1 slow-fade"><i class="play-video fas fa-caret-square-right"></i></div>
-				
-				<div class="heading__light slow-fade hero__copy font300 mb2"><?php the_field( 'hero_copy' );?></div>
-				
-				<?php if( have_rows('button') ): ?>
-
-				<div class="action_buttons">
-			
-					<?php $count = 0;
+						<img class="slide-down img-logo" src="<?php echo get_template_directory_uri() . "/inc/img/letaka-logo.svg"; ?>">
 						
-					while( have_rows('button') ): the_row(); ?>
+						<h1 class="heading heading__xl heading__light"><?php the_field( 'hero_heading' );?></h1>
+					
+					</div>
+			
+					<div>
+					
+						<div class="video-icon mt1 mb1 slow-fade"><i class="play-video fas fa-caret-square-right"></i></div>
 						
-					<a class="button slide-up <?php if($count > 0) echo "button__transparent-light"; ?>" href="<?php the_sub_field("button_target"); ?>"><?php
-						the_sub_field("button_text");
-					?></a>
-				
-					<?php $count++; endwhile; ?>
-			
-				</div>
-			
-				<?php endif; ?>
-				
-			</div>
+						<div class="heading__light slow-fade hero__copy font300 mb2"><?php the_field( 'hero_copy' );?></div>
+						
+						<?php if( have_rows('button') ): ?>
+		
+						<div class="action_buttons">
+					
+							<?php $count = 0;
+								
+							while( have_rows('button') ): the_row(); ?>
+								
+							<a class="button slide-up <?php if($count > 0) echo "button__transparent-light"; ?>" href="<?php the_sub_field("button_target"); ?>"><?php
+								the_sub_field("button_text");
+							?></a>
+						
+							<?php $count++; endwhile; ?>
+					
+						</div>
+					
+						<?php endif; ?>
+						
+					</div>
        
-        </div>
+        		</div>
+        		
 			</div>
 			
         <?php else: ?>
         
         	<div class="col-12 col-sm-9 offset-0 offset-sm-3">
-                <div class="hero__content">
+                
+				<div class="hero__content">
 		
-		<h1 class="heading heading__xl heading__light slide-up"><?php the_field( 'hero_heading' );?></h1>
+					<h1 class="heading heading__xl heading__light slide-up"><?php
+						
+						$category_name = get_query_var("category_name");
+						
+						if($category_name) {
+							echo get_category_by_slug($category_name)->name;
+						} else {
+							the_field( 'hero_heading' );
+						}
+					
+					?></h1>
 		
-		<div class="heading__light slow-fade hero__copy mt1"><?php the_field( 'hero_copy' );?></div>
-   
-    </div>
+					<div class="heading__light slow-fade hero__copy mt1"><?php the_field( 'hero_copy' );?></div>
+			   
+			    </div>
+			    
             </div>
             
         <?php endif; ?>       
