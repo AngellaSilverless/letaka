41fdd56869df0ea1c7d738de941eb803183360ee
Ash - SIlverless <ash@silverless.co.uk>
2019-12-02 16:23:04 +0000

    DEV - 404

--- 404.php
+++ 404.php
@@ -6,9 +6,18 @@
  */
 get_header();
 ?>
-	<div class="404-wrapper">
-		<h1 class="heading heading__xl heading__light">There is an error.</h1>
-		<p>Try navigating using one of the links above</p>
+	<div class="404-wrapper"  style="	height:100vh;
+		display:grid;
+		align-items:center;
+		text-align:center;
+		background:#3d1a1a;
+		color: white;
+		cursor: initial;"">
+		<div>
+		<h1 class="heading heading__xl heading__light">Sorry,</h1>
+		<h2 class="heading heading__lg heading__light">there has been a slight error.</h1>
+		<p class="mt3" style="color:white;">Try navigating using one of the links above</p>
+		</div>
 	</div>
 <?php
 get_footer();
